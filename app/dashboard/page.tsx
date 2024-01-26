9d0a1b8fa7c680c68e88f5a61b88d1f1977ee10b
Sid <sidsurakanti1@gmail.com>
2024-01-26 00:53:47 -0500

    added boilerplate code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard () {
+    return (
+        <main className="min-h-screen">
+            Dashboard
+        </main>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+
+
+const inter = Inter({
+    subsets: ["latin"]
+});
+const poppins = Poppins({
+    weight: ["400", "500", "600"],
+    subsets: ["latin"],
+});
+
+
+export { inter, poppins };
