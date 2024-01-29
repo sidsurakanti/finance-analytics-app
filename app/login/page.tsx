ff47e36008fb42035ca6c59ce7d41c953c9364b3
Sid <sidsurakanti1@gmail.com>
2024-01-28 22:46:47 -0500

    populated sign up page to some extent

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/app/ui/LoginForm"
 
 export default function Login () {
     return (
-        <main className="min-h-screen w-4/5 mx-auto">
-            <p>Sign up</p>
+        <main className="min-h-screen w-4/5 mx-auto flex flex-col items-center space-y-4">
+            <p className="text-3xl p-4">Sign up</p>
             <LoginForm />
         </main>
     )
