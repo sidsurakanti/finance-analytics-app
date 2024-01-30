788269fe439c64cd4b95935489cccfd821c60f57
Sid <izzyy.wq@gmail.com>
2024-01-30 16:44:58 -0500

    added updateCashflows function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import SideBar from "@/app/ui/SideBar"
-import { fetchCashflows, fetchUser } from "@/app/lib/data"
+import { fetchCashflows, fetchUser, updateCashflows } from "@/app/lib/data"
 
 export default async function Dashboard () {
     const user = await fetchUser('johndoe@gmail.com');
     const cashflows = await fetchCashflows(user);
+    const newIncome = 4000;
+    const res = await updateCashflows({user, newIncome})
 
     console.log(user);
     console.log(cashflows);
+    console.log(res)
 
     return (
         <main className="min-h-screen w-4/5 mx-auto">
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -25,10 +25,28 @@ export async function fetchUser (email: string) {
             WHERE email='johndoe@gmail.com';
         `;
         const user = data.rows[0];
-        console.log(user)
         return user;
     } catch (error) {
         console.log("Database error", error);
         throw new Error("Failed to fetch user");
+    }
+}
+
+type updaterProps = {
+    user: User,
+    newIncome: Number,
+}
+
+export async function updateCashflows ({user, newIncome}: updaterProps) {
+    try {
+        const res = await sql`
+            UPDATE cashflows 
+            SET income = ${newIncome.toString()}
+            WHERE user_id = ${user.id?.toString()};
+        `;
+        return res.rows;
+    } catch (error) {
+        console.log("Database error", error);
+        throw new Error("Failted to update cashflows");
     }
 }
