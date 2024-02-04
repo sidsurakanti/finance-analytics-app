892d23e2b9716d547e80a6f8381bb381044cce1c
Sid <sidsurakanti1@gmail.com>
2024-02-04 00:10:43 -0500

    impr(addTransaction): added basic skeleton for processing form data

--- app/ui/components/dashboard/TransactionDialogue.tsx
+++ app/ui/components/dashboard/TransactionDialogue.tsx
@@ -1,12 +1,28 @@
+"use client"
+
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   DialogTrigger,
   DialogHeader,
+  DialogClose,
 } from "@/ui/components/Dialogue";
+import { useForm, SubmitHandler} from "react-hook-form"
+
+
+// TODO: convert this using zod
+interface Inputs {
+  name: string,
+  amount: Number,
+}
 
 export default function TransactionDialogue() {
+  const { register, handleSubmit } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs>= (data) => {
+    console.log(data);
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -19,10 +35,15 @@ export default function TransactionDialogue() {
           <DialogTitle>Add Transactions</DialogTitle>
         </DialogHeader>
 
-        <div>
-          <input type="text" placeholder="Name" />
-          <input type="number" placeholder="Amount" />
-        </div>
+        <form className="flex flex-col space-y-3" onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-x-2">
+            <input className="outline-none rounded-lg bg-[#2b2727] p-2" {...register("name", {required: true}) } placeholder="Name"/>
+            <input className="outline-none rounded-lg bg-[#2b2727] p-2" {...register("amount", {required: true})} placeholder="Amount" />
+          </div>
+          <div>
+              <input className="bg-blue-500 p-2 rounded-xl" type="submit" /> 
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
