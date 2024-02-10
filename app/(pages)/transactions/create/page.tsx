848f32afb6bd2ada0eae9c460d5146ab731e3fbf
Sid <sidsurakanti1@gmail.com>
2024-02-09 19:21:07 -0500

    build(shadcn/ui): added shadcn ui

--- app/(pages)/transactions/create/page.tsx
+++ app/(pages)/transactions/create/page.tsx
@@ -1,7 +1,7 @@
 import { CreateTransactionForm } from "@components/transactions/CreateTransactionForm";
 import { fetchUser } from "@lib/data";
 
-export default async function Page() {
+export default async function CreateTransaction() {
   // * temporary solution until auth is added
   const user = await fetchUser("janedoe@gmail.com");
 
