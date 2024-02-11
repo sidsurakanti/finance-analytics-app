f73e773aced632131e82d8e1cd19a1ac50a4ca42
Sid <sidsurakanti1@gmail.com>
2024-02-11 18:19:10 -0500

    fix(cashflow edit): fixed cashflow edit button not showing up on light mode

--- components/cashflows/CashflowCard.tsx
+++ components/cashflows/CashflowCard.tsx
@@ -9,12 +9,12 @@ export function CashflowCard({ title, value }: Props) {
   return (
     <div className="h-min-fit w-full bg-accent rounded-lg p-4 flex flex-col justify-between gap-3 md:gap-20 border border-border">
       <p className="text-2xl">{title}</p>
-      <div
+      <span
         className={`${inter.className} text-4xl md:text-5xl 2xl:text-[55px] flex flex-row items-end gap-[3px] font-medium`}
       >
         <p className="text-muted-foreground">$</p>
-        {value}
-      </div>
+        <p>{value}</p>
+      </span>
     </div>
   );
 }
--- components/cashflows/EditButtonWrapper.tsx
+++ components/cashflows/EditButtonWrapper.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@components/ui/button";
 import { EditIcon } from "@components/ui/icons";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 
 interface WrapperProps {
@@ -10,11 +12,23 @@ interface WrapperProps {
 }
 
 export function Wrapper({ children, editText }: WrapperProps) {
+  const { theme } = useTheme();
+
   return (
     <>
-      <Link href="/cashflows/edit" className="flex justify-end pb-2">
-        <Button className="flex flex-row gap-2" variant="link">
-          <EditIcon width={20} height={20} />
+      <Link href="/cashflows/edit" className="flex justify-start pb-2">
+        <Button
+          className={cn(
+            "flex flex-row gap-2",
+            theme == "dark" ? "hover:bg-primary" : "hover:bg-accent/75",
+          )}
+          variant="link"
+        >
+          <EditIcon
+            className={cn(theme == "light" && "text-black")}
+            width={20}
+            height={20}
+          />
           {editText && <p className="text-lg hidden md:block">Edit</p>}
         </Button>
       </Link>
