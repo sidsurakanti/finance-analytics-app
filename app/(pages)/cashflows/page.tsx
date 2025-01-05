a03f9d1e73994356581b21bb7ff05580821a33c1
sidsurakanti <izzyy.wq@gmail.com>
2025-01-05 00:11:57 -0500

    refactor(cashflows): update paycheck details handling for checking balance card

--- app/(pages)/cashflows/page.tsx
+++ app/(pages)/cashflows/page.tsx
@@ -1,9 +1,9 @@
-import BalanceShow from "@/components/cashflows-page/CashflowCards";
+import CashflowCards from "@/components/cashflows-page/CashflowCards";
 
 export default function CashflowsPage() {
   return (
     <main className="h-full w-[90%] md:w-[85%] 2xl:w-4/5 mx-auto">
-      <BalanceShow />
+      <CashflowCards />
     </main>
   );
 }
