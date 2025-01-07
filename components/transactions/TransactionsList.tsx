91854b5b46f64814d8dcb1aaa56603bd88385cbd
sidsurakanti <izzyy.wq@gmail.com>
2025-01-07 16:51:02 -0500

    fix: fixed bug where create transaction sheet wouldn't render

--- components/transactions/TransactionsList.tsx
+++ components/transactions/TransactionsList.tsx
@@ -2,23 +2,25 @@ import { fetchAllTransactions, fetchReoccuring } from "@/lib/data";
 import { DataTable } from "@/components/transactions/DataTable";
 import { User, Transaction, Reoccuring } from "@/lib/definitions";
 import { columns } from "@/components/transactions/TransactionColumns";
-import { TransactionSheet } from "@/components/transactions/CreateTransaction";
 import { auth } from "@/auth";
 
 export default async function TransactionList() {
   const session = await auth();
   const user = session?.user as User;
   const transactions: Transaction[] = await fetchAllTransactions(user);
-  // fetch reoccuring transactions to pass to the CreateTransactionForm
+  // fetch reoccuring transactions to pass to the CreateTransactionForm and EditTransactionForm
   // we need this for when the user is creating a transactions thats also a reoccuring transaction
   const reoccuring: Reoccuring[] = await fetchReoccuring(user);
   // console.log(transactions);
 
   return (
     <section>
-      <DataTable columns={columns} data={transactions} reoccuring={reoccuring}>
-        <TransactionSheet user={user} reoccuring={reoccuring} />
-      </DataTable>
+      <DataTable
+        columns={columns}
+        data={transactions}
+        reoccuring={reoccuring}
+        user={user}
+      />
     </section>
   );
 }
