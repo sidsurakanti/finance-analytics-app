325f7207d351222cac80776566ad0c3602863f91
sidsurakanti <izzyy.wq@gmail.com>
2025-01-13 20:50:23 -0500

    feat(auto-paychecks): make logic more robust and update it to log added paychecks in transactions

--- components/cashflows/income/PaychecksSyncToaster.tsx
+++ components/cashflows/income/PaychecksSyncToaster.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useToast } from "@/components/ui/hooks/use-toast";
-import { addMissedPaychecks, checkForMissedPaychecks } from "@/lib/actions";
+import {
+  type MissedPaycheckT,
+  addMissedPaychecks,
+  checkForMissedPaychecks,
+} from "@/lib/actions";
 import { IncomeSources, User } from "@/lib/definitions";
 import { cashFormatter } from "@/lib/utils";
 import { useState, useEffect } from "react";
@@ -20,43 +24,54 @@ export default function PaychecksSyncToaster({
   const [hasChecked, setHasChecked] = useState<boolean>();
 
   useEffect(() => {
-    if (!hasChecked) {
-      const func = async () => {
-        const missedPaychecks: number[][] = await checkForMissedPaychecks(
-          incomeSources,
-          lastPaycheckSync,
-        );
-        // console.log(missedPaychecks);
-        if (
-          missedPaychecks.length === 2 &&
-          missedPaychecks.every(
-            (arr) => arr.length === 2 && arr[0] === 0 && arr[1] === 0,
-          )
-        ) {
-          setHasChecked(true);
-          return;
-        }
-        await addMissedPaychecks(missedPaychecks, user.id);
+    let isMounted = true;
+    if (hasChecked) return;
 
-        missedPaychecks.forEach(([missedPaychecks, missedIncome]) => {
-          const description =
-            missedPaychecks > 1
-              ? `We've noticed you missed ${missedPaychecks} paycheck(s), totaling ${cashFormatter(missedIncome)}. We've added this amount to your checking balance!`
-              : `Your latest paycheck of ${cashFormatter(missedIncome)} has been added to your balance.`;
+    const func = async () => {
+      if (!isMounted) return;
+      // get missed paycheck data for each income source a user has
+      const missedPaychecks: MissedPaycheckT[] = await checkForMissedPaychecks(
+        incomeSources,
+        lastPaycheckSync,
+      );
+      // console.log(missedPaychecks);
 
-          toast({
-            title: "You've been paid!",
-            description: description,
-            action: <ToastAction altText="dismiss">dismiss</ToastAction>
-          });
-        });
-
+      // if user hasn't missed any paychecks for all incomeSources
+      if (missedPaychecks.every((arr) => arr.missedPaychecksCount < 1)) {
         setHasChecked(true);
-      };
+        return;
+      }
 
-      func();
-    }
-  });
+      // missed paychecks details per income source
+      // len of missedPaychecks = incomeSources.length
+      for (const missed of missedPaychecks) {
+        const { incomeSource, missedPaychecksCount, missedIncome } = missed;
+
+        if (missedPaychecksCount === 0) continue;
+
+        await addMissedPaychecks(missed, user.id);
+
+        const description =
+          missedPaychecksCount > 1
+            ? `Missed ${missedPaychecksCount} paycheck(s) from ${incomeSource.name}, totaling ${cashFormatter(missedIncome)}. Added to your balance!`
+            : `Your latest paycheck of ${cashFormatter(missedIncome)} from ${incomeSource.name} has been added to your balance.`;
+
+        toast({
+          title: "You've been paid!",
+          description,
+          action: <ToastAction altText="dismiss">dismiss</ToastAction>,
+        });
+      }
+
+      setHasChecked(true);
+    };
+
+    func();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [hasChecked, incomeSources, lastPaycheckSync, user.id, toast]);
 
   return null;
 }
